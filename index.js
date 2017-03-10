6456e9891dd0b915acb1b5ed8b748898ac790558
Valentin Radulescu <radulescu.valentin@gmail.com>
2017-03-11 01:23:14 +0200

    Update date format

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const rp = require('request-promise');
 const xml2js = require('xml2js');
 const express = require('express');
+const dateFormat = require('dateformat');
 const app = express();
 
 const RSS_URL = 'http://www.animenewsnetwork.com/all/rss.xml';
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (appReq, appRes) => {
   rp(RSS_URL)
@@ -13,9 +14,11 @@ app.get('/', (appReq, appRes) => {
       let alexaNewsItems = [];
 
       rssJSON.rss.channel[0].item.forEach(item => {
+        const itemDate = new Date(item.pubDate[0]);
+
         alexaNewsItems.push({
           uid: item.guid[0]._,
-          updateDate: item.pubDate[0],
+          updateDate: dateFormat(itemDate, `UTC:yyyy-mm-dd'T'HH:MM:ss'.0Z'`),
           titleText: item.title[0],
           mainText: item.description[0],
           redirectionURL: item.link[0]
