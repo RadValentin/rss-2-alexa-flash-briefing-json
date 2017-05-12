1c284fa46104272d47c662ab30f180df0561e4cf
Valentin Radulescu <radulescu.valentin@gmail.com>
2017-05-12 21:30:37 +0300

    Store feed in memory and update at fixed intervals

--- index.js
+++ index.js
@@ -5,37 +5,43 @@ const dateFormat = require('dateformat');
 const app = express();
 
 const RSS_URL = 'http://www.animenewsnetwork.com/all/rss.xml';
+const RSS_POLL_TIMEOUT = 15 * 60 * 1000
+let dataCache = {};
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (appReq, appRes) => {
-  rp(RSS_URL)
-    .then(rssRes => xml2js.parseString(rssRes, (err, rssJSON) => {
-      let alexaNewsItems = [];
-
-      rssJSON.rss.channel[0].item.forEach(item => {
-        const itemDate = new Date(item.pubDate[0]);
-
-        alexaNewsItems.push({
-          uid: item.guid[0]._,
-          updateDate: dateFormat(itemDate, `UTC:yyyy-mm-dd'T'HH:MM:ss'.0Z'`),
-          titleText: item.title[0],
-          mainText: item.description[0],
-          redirectionURL: item.link[0]
-        });
-      });
-
-      appRes.json(alexaNewsItems);
-    }))
-    .catch(err => {
-      console.error(err);
-
-      appRes.json({
-        err
-      });
-    });
+  appRes.json(dataCache);
 });
 
 app.listen(app.get('port'), function() {
   console.log('RSS proxy running on port', app.get('port'));
 });
+
+function updateDataCache() {
+  rp(RSS_URL)
+    .then(rssRes =>
+      xml2js.parseString(rssRes, (err, rssJSON) => {
+        let alexaNewsItems = [];
+
+        rssJSON.rss.channel[0].item.forEach(item => {
+          const itemDate = new Date(item.pubDate[0]);
+
+          alexaNewsItems.push({
+            uid: item.guid[0]._,
+            updateDate: dateFormat(itemDate, `UTC:yyyy-mm-dd'T'HH:MM:ss'.0Z'`),
+            titleText: item.title[0],
+            mainText: item.description[0],
+            redirectionURL: item.link[0]
+          });
+        });
+        dataCache = alexaNewsItems;
+      })
+    )
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+updateDataCache();
+setInterval(updateDataCache, RSS_POLL_TIMEOUT);
